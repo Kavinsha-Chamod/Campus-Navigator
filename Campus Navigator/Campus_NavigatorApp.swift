c2b8c92a2c9409f78d9ed5052f55b252d3154c6d
Kavinsha <kavinshachamod2001@gmail.com>
2025-02-16 10:40:28 +0530

    Initial Commit

---
+++ Campus Navigator/Campus_NavigatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Campus_NavigatorApp.swift
+//  Campus Navigator
+//
+//  Created by Kavinsha 021 on 2025-02-16.
+//
+
+import SwiftUI
+
+@main
+struct Campus_NavigatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Campus Navigator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Campus Navigator
+//
+//  Created by Kavinsha 021 on 2025-02-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
